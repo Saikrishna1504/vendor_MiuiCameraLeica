71d2491743a03a8ae5b44e9139a7c2913ef3bd29
Saikrishna1504 <saikrishna26918@gmail.com>
2023-11-28 07:29:09 +0000

    begonia: Import MiuiCameraLeica
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

---
+++ system/priv-app/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := MiuiCameraLeica
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := MiuiCameraLeica/MiuiCameraLeica.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+LOCAL_OVERRIDES_PACKAGES := SnapdragonCamera Snap Camera2
+include $(BUILD_PREBUILT)
