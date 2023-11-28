6dbcb95ff32ce8a0edff8953312e38db3d7780f0
Saikrishna1504 <saikrishna26918@gmail.com>
2023-11-28 18:06:57 +0530

    begonia: Fix typo
    * I have used ANX Template ,so now i have updated correctly
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- config.mk
+++ config.mk
@@ -16,7 +16,7 @@ VENDOR_PATH := vendor/MiuiCameraLeica
 
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc)  \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/ANXCamera/lib/arm64,system/priv-app/ANXCamera/lib/arm64)
+	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiCameraLeica/lib/arm64,system/priv-app/MiuiCameraLeica/lib/arm64)
 
 PRODUCT_PACKAGES += \
     MiuiCameraLeica
