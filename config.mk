2059e8f6dd0929e6011c6ee5a2e9b0436ae16eb3
Saikrishna1504 <saikrishna26918@gmail.com>
2023-12-02 04:51:23 +0000

    begonia: Add MiuiExtraPhoto.apk
    * Helps for document mode
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- config.mk
+++ config.mk
@@ -16,7 +16,10 @@ VENDOR_PATH := vendor/MiuiCameraLeica
 
 PRODUCT_COPY_FILES += \
 	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc)  \
-	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiCameraLeica/lib/arm64,system/priv-app/MiuiCameraLeica/lib/arm64)
+	$(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiCameraLeica/lib/arm64,system/priv-app/MiuiCameraLeica/lib/arm64) \
+        $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/priv-app/MiuiExtraPhoto/lib/arm64,system/priv-app/MiuiExtraPhoto/lib/arm64)
 
 PRODUCT_PACKAGES += \
-    MiuiCameraLeica
+    MiuiCameraLeica \
+    MiuiExtraPhoto 
+
